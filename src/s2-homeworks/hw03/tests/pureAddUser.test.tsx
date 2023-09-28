4c9137a83fac3e91581a178c732c04c0aa8957a8
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-09-28 08:50:58 +0300

    homework sprint 2 week 3

--- src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {pureAddUser} from '../GreetingContainer'
 
-let name: any
-const setName = (a: any) => {
+let name: string
+const setName = (a: string) => {
     name = a
 }
-let error: any
-const setError = (a: any) => {
+let error: string
+const setError = (a: string) => {
     error = a
 }
-let added: any
+let added: boolean
 const addUserCallback = () => {
     added = true
 }
@@ -31,13 +31,13 @@ test('name 2', () => {
     name = ''
     pureAddUser(name, setError, setName, addUserCallback)
     expect(name).toBe('')
-    expect(error).toBe('Ошибка! Введите имя!')
+    expect(error).toBe('PLease enter your name...')
     expect(added).toBe(false)
 })
 test('name 3', () => {
     name = '    '
     pureAddUser(name, setError, setName, addUserCallback)
-    expect(name).toBe('    ')
-    expect(error).toBe('Ошибка! Введите имя!')
-    expect(added).toBe(false)
+    expect(name).toBe('')
+    expect(error).toBe('')
+    expect(added).toBe(true)
 })
