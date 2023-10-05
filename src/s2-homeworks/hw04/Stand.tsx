5deb053e53d768ca82e29bd10c801217e90a13c6
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-10-05 07:14:58 +0300

    homework sprint 2 week 4

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -90,6 +90,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
+                        //onChangeChecked={setChecked}
                         onChange={(e) => setChecked(e.currentTarget.checked)}
                     />
                 </div>
--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,6 +27,10 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e);
+        onChangeChecked?.(e.currentTarget.checked);
+
+
         // задачка на написание онченджа
 
     }
