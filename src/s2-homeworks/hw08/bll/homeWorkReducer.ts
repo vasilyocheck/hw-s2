e3d8bd38839ee6e3e0883f17e53a67e705de0266
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-10-30 09:36:49 +0300

    homework sprint 3 week 4 (hw8)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if(action.payload === 'up') {
+                const newState = state.map(u => u);
+                return newState.sort((a, b) => (a.name > b.name) ? 1 : -1) // need to fix
+            }
+            if(action.payload === 'down') {
+                const newState = state.map(u => u);
+                return newState.sort((a, b) => (b.name > a.name) ? 1 : -1) // need to fix
+            }
+            return state;
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
